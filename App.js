b8c10dac727e4216dec7e70b14cb95faea3a0c45
Mahrukh Aqdas <90785391+sp19-bse-039-c@users.noreply.github.com>
2021-12-29 01:18:22 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>This is my first react app</h1> 
+      <button variant="primary">bootsrtap button</button>
+       <button variant="secondary">bootsrtap button</button>
+      <p>hjbadygudfijfvhdijh;ahkagilajlyuaghtudygiudhuywgyudhiwdhkuaydgiwdJEUYDHYC9UJHWDKYGWAIHUJ</p>
+    </div>
+  );
+}
+
+export default App;
